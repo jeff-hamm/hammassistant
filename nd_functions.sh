ddbfdce677611af7def323aaeb827dd2fcb89f14
Jeff Hamm <hammj@opentext.com>
2025-03-11 08:58:54 -0700

    added trap

--- nd_functions.sh
+++ nd_functions.sh
@@ -3,18 +3,19 @@ nd_name=${nd_name:-"netdaemon5"}
 nd_slug=${nd_slug:-"c6a2317c_$nd_name"}
 nd_path=${nd_path:-"$config_root/$nd_name"}
 nd_src=${nd_src:-$config_root/hammassistant}
+
 #nd_config_path=${nd_config_path}
 function logs() {
     ha host logs --identifier addon_$nd_slug $@
 }
 
 function restart() {
-    trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
     logs -f &
-    background_pid=$!
+    export background_pid=$!
+    trap "trap - SIGINT SIGTERM EXIT && kill $background_pid" SIGINT SIGTERM EXIT
     bashio $nd_src/restart.sh
     sleep 10
-    kill $background_pid
+    export background_pid=
 }
 
 function publish() {
